deae322dba9e3878e5112b21b8257abe8801e418
laelaps <77897569+lae-laps@users.noreply.github.com>
2022-03-03 17:59:55 +0100

    Update launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,7 +1,11 @@
-killall -q polybar
+#!/bin/bash
 
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload
-done
-echo "Bars launched..."
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar full 2>&1 | tee -a /tmp/polybar.log & disown
+
+echo "Polybar launched..."
